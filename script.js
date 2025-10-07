727c38992489bc8b4d2eeecbf395d67e3a4cc2c1
AndresISouto <AndresISouto@users.noreply.github.com>
2025-10-07 10:21:00 +0200

    fix folder structure

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () =>{
-    const menuBtn = document.querySelector(".hamburgesa")
-    const enlaces = document.querySelector("#enlaces")
-
-    menuBtn.addEventListener("click",()=> {
-        enlaces.classList.toggle("activo")
-    })
-})
